5f49932a0264288cb60a161eabcb96404220e08c
Felice Forby <feliceforby@gmail.com>
2019-03-28 22:22:08 +0000

    Automatically backed up by Learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import BandInput from './BandInput';
-import Band from './Band';
+import Bands from './Bands';
 
 import { connect } from 'react-redux'
 
@@ -10,9 +10,7 @@ class BandsContainer extends Component {
     return (
       <div>
         <BandInput addBand={this.props.addBand}/>
-        <ul>
-          {this.renderBands()}
-        </ul>
+        <Bands bands={this.props.bands} delete={this.props.deleteBand} />
       </div>
     )
   }
