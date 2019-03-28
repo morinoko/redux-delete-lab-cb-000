3954665a23e7f56323caf9019f37e64b783a0ef1
Felice Forby <feliceforby@gmail.com>
2019-03-28 22:23:16 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Band from './Band';
 
 const Bands = props => {
-  const bands = props.bands.map(band => <Band key={band.id} band={band} delete={props.deleteBand} />);
+  const bands = props.bands.map(band => <Band key={band.id} band={band} delete={props.delete} />);
   
   return (
     <ul>
