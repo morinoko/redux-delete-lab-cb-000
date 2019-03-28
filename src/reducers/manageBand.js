b879c9c6c23c5e9d550c7f95c7d0e292479b218a
Felice Forby <forby.1@gmail.com>
2019-03-29 07:13:00 +0900

    Add delete functionality

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,9 +3,21 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      const newBand = {
+        name: action.name,
+        id: Math.random()*1000000000
+      }
+      
+      return { 
+        ...state,
+        bands: [...state.bands, newBand]
+      }
+    
+    case 'DELETE_BAND':
+      const filteredBands = state.bands.filter(band => band.id !== action.id)
+      
+      return { bands: filteredBands };
+      
     default:
       return state;
   }
