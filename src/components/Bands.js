4dab4550d206d19f7f71c78fa57433661e795c69
Felice Forby <feliceforby@gmail.com>
2019-03-28 22:20:34 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -4,6 +4,6 @@ import Band from './Band';
 const Bands = props => {
   const bands = props.bands.map(band => {
       return <Band key={band.id} band={band} delete={props.deleteBand} />
-    });
-  }
+    }
+  );
 }
