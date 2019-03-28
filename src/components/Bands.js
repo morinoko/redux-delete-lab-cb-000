6477ac9f346a35b31125d46e86acbc39544028c1
Felice Forby <feliceforby@gmail.com>
2019-03-28 22:20:04 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,5 +2,8 @@ import React, { Component } from 'react';
 import Band from './Band';
 
 const Bands = props => {
-  const bands = props.bands.map
+  const bands = props.bands.map(band => {
+      return <Band key={band.id} band={band} delete={props.deleteBand} />
+    });
+  }
 }
